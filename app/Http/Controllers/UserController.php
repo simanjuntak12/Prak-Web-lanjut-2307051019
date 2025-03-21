a40d3768c7a41407ddb72b927dcaf32945c58916
simanjuntak12 <simanjuntaka318@gmail.com>
2025-03-21 14:53:53 +0700

    database-model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,21 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Kelas;
+use App\Http\Requests\UserRequest;
+use App\Models\UserModel;
+use App\Models\User;
+
 
 class UserController extends Controller
 {
-        public function create(){ 
-          
-            return view('create_user');        
-        }
-        public function store(Request $request)
-{
-    $data = [ 
-        'nama' => $request->input('nama'), 
-        'kelas' => $request->input('kelas'), 
-        'npm' => $request->input('npm'), 
-        ]; 
+  
+    public function store(UserRequest $request)
+    {
+        $validateData = $request->validate([
+            'nama' => 'required|string|max:255',
+            'npm' => 'required|string|max:255',
+            'kelas_id' => 'required|exists:kelas,id',
+        ]);
 
-   return view('profile', $data);
-}
+        $user = UserModel::create($validateData);
+
+        $user->load('kelas');
+
+        return view('profile', [
+            'nama' => $user->nama,
+            'npm' => $user->npm,
+            'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
+    ]);
+
     }
+
+    public function create()
+    {
+        $kelasModel = new Kelas();
+
+        $kelas = Kelas::all(); // Ambil semua data Kelas
+
+        $data = [
+            'title' => 'Create User',
+            'kelas' => $kelas,
+        ];
+
+        return view('create_user', $data);
+    }
+}
